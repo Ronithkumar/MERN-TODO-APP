7644b29a9e7584e0b3c2f20560c9adb8a64cf5ce
Ronith Kumar <73122974+Ronithkumar@users.noreply.github.com>
2025-05-18 11:36:50 +0530

    updating env in config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 const config = {
-  API_BASE_URL: "http://localhost:4000",
-  TOKEN_KEY: "authToken", // centralized key name for token storage
+  API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "http://localhost:4000",
+  TOKEN_KEY: "authToken",
 };
 
 export default config;
